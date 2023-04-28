9282db64036eeb5c89455585df67d371f5b29d69
11t-kato <taq11081108p@gmail.com>
2023-04-28 14:48:52 +0900

    変数変更しました

--- STFT.m
+++ STFT.m
@@ -5,23 +5,21 @@ info = audioinfo("kitamuravoice.wav"); % オーディオファイル情報の取
 % t = 0:seconds(1/samplefs):seconds(info.Duration); % 経過時間を表すyと
 % t = t(1:end-1); % 同じ長さのベクトル t を作成
 
-y1 = y(:,1); %kitamuravooice Lch抽出
+voiceSig = y(:,1); % kitamuravooice Lch抽出
 
-L = 1024; %ウィンドウの長さ
-shiftLen = L/2; %シフト幅
-W = hann(L); %ハンウィンドウ
-A = zeros(shiftLen,1); % 先頭に0行列追加
-B = zeros(L,1); % 最後に0行列追加
-b = [A; y1]; % 先頭に0行列⁺オーディオデータ
-sigLen = size(b, 1); % サイズ表示
-y1 = [A; y1; B]; % 連結
-J = ceil(sigLen/shiftLen)-1; %フレーム分割数
-x = zeros(L, J); %から行列
+winLen = 1024; % ウィンドウの長さ
+shiftLen = winLen/2; % シフト幅
+win = hann(winLen); % ハンウィンドウ
+padSig = [zeros(shiftLen, 1); voiceSig]; % 先頭に0行列⁺オーディオデータ
+sigLen = size(padSig, 1); % サイズ表示
+padSig = [padSig; zeros(winLen-1, 1)]; % 先頭0オーディオデータ⁺最後に0行列
+J = ceil(sigLen/shiftLen)-1; % フレーム分割数
+spec = zeros(winLen, J); % から行列
 
 for i=1:J % 1~J回処理の繰り返し
     startPoint = shiftLen*(i - 1) + 1; % 初めの点
-    tmp = y1(startPoint:startPoint+(L-1), :); % 指定範囲の点をとる
-    a = tmp.*W; % y要素の掛け算
-    x1 = fft(a); % 短時間フーリエ変換
-    x(:, i) = x1; % 作成したから行列
+    tmp = padSig(startPoint:startPoint+(winLen-1), :); % 指定範囲の点をとる
+    tmp1 = tmp .* win; % y要素の掛け算
+    tmp2 = fft(tmp1); % 短時間フーリエ変換
+    spec(:, i) = tmp2; % 作成したから行列
 end
--- main.m
+++
@@ -1,7 +0,0 @@
-time = 10;
-samplefs = 44100;
-fs = 440;
-t = linspace(0,10,time * samplefs);
-omega = 2 * pi * fs;
-y = sin(omega * t);
-plot(t,y);
